d28a7bd738729a6b455748bde4668902fc5265c2
Seongjun Jang <41983244+junnei@users.noreply.github.com>
2023-09-02 14:04:22 +0900

    [Chore] modify files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 import openai
 import json
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, jsonify
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -17,16 +17,16 @@ Please write a first-person game scenario, and always gives me 3 options the mai
 Please talk from a first-person perspective instead of using the expression "game" to make the user feel immersed.
 And I hope there's a conversation with other characters.
 And I want you to proceed with the story according to my choice.
-Also, tell me the main character's HP(Health Point). HP starts at 5. HP is always between 0 and 5.
-Depending on my choice, HP can drop or recover.
-When HP drops below zero, the main character dies and the game is over.
-Tell me the HP number first, and then tell me the story and the options.
+Also, tell me the main character's hp(Health Point). hp starts at 5. hp is always between 0 and 5.
+Depending on my choice, hp can drop or recover.
+When hp drops below zero, the main character dies and the game is over.
+Tell me the hp number first, and then tell me the story and the options.
 Only When the game is over, put <END> at the end in content.
 And answer in json form. 
 For example, json would be like this. 
 {
-    "ID":1,
-    "HP":5,
+    "id":1,
+    "hp":5,
     "background": "The hero who appeared as a warrior in a fantasy world full of magic and monsters",
     "theme":"story book, digital art",
     "content":"안녕하세요! 이제부터 시작하는 모험 게임에 오신 것을 환영합니다. 이 게임은 여러분이 영웅이 되어 다양한 도전과 과제를 수행하는 것을 목표로 합니다. 게임 세계는 마법과 몬스터로 가득 찬 판타지 세계입니다. 여러분의 할일은 영웅의 역할을 맡아 세계를 구하는 것입니다. 어떻게 하시겠습니까?",
@@ -38,8 +38,8 @@ For example, json would be like this.
     },
     "state":"playing"
 }
-The ID value means the order in which you answered. And background means a description of the picture that describes the situation of the content well. And background is short description for the situation of content's story. And the background have to be written in English less than 80 characters. The theme is picture's style, and it will be used as a prompt for Dalle-2, and the Dalle-2 will have the same value as "story book, digital art" to print a prettier picture.
-And the HP, content with stories, and choices with 3 options will be included in json.
+The id value means the order in which you answered. And background means a description of the picture that describes the situation of the content well. And background is short description for the situation of content's story. And the background have to be written in English less than 80 characters. The theme is picture's style, and it will be used as a prompt for Dalle-2, and the Dalle-2 will have the same value as "story book, digital art" to print a prettier picture.
+And the hp, content with stories, and choices with 3 options will be included in json.
 And state has a value of one of playing, fail, and success. If you survive the game, you'll have success, if you die with zero stamina, you'll have fail, and the rest of playing.
 
 Please fill out the content with up to 500 characters maximum when in English.
@@ -53,8 +53,8 @@ If the user asks a question other than the option, don't answer it, but ask them
     {
         "role": "assistant",
         "content": '''{
-    "ID":1,
-    "HP":5,
+    "id":1,
+    "hp":5,
     "background": "The hero who appeared as a warrior in a fantasy world full of magic and monsters",
     "theme":"story book, digital art",
     "content":"안녕하세요! 이제부터 시작하는 모험 게임에 오신 것을 환영합니다. 이 게임은 여러분이 영웅이 되어 다양한 도전과 과제를 수행하는 것을 목표로 합니다. 게임 세계는 마법과 몬스터로 가득 찬 판타지 세계입니다. 여러분의 할일은 영웅의 역할을 맡아 세계를 구하는 것입니다. 어떻게 하시겠습니까?",
@@ -74,6 +74,21 @@ If the user asks a question other than the option, don't answer it, but ask them
     ]
 
 messages = []
+
+temp =[{
+    "id":1,
+    "hp":5,
+    "background": "The hero who appeared as a warrior in a fantasy world full of magic and monsters",
+    "theme":"story book, digital art",
+    "content":"안녕하세요! 이제부터 시작하는 모험 게임에 오신 것을 환영합니다. 이 게임은 여러분이 영웅이 되어 다양한 도전과 과제를 수행하는 것을 목표로 합니다. 게임 세계는 마법과 몬스터로 가득 찬 판타지 세계입니다. 여러분의 할일은 영웅의 역할을 맡아 세계를 구하는 것입니다. 어떻게 하시겠습니까?",
+    "choices":
+    {
+        "a":"모험을 시작한다!",
+        "b":"게임 규칙을 알려주세요.",
+        "c":"이 게임은 어떻게 진행되나요?"
+    },
+    "state":"playing"
+}]
 
 def summarize(openai: any, messages: list) -> str:
     content = ''''''
@@ -166,6 +181,4 @@ def command():
 
 @app.route("/hello", methods=['GET'])
 def hello():
-  a = {"body" : "data",
-       "title" :"text"}
-  return json.loads(a)
+  return jsonify(temp)
